702fd9e5a028d726d9662bd508fcee1b7ef7d752
YITZHAK ALVAREZ <yitzhak.alvarez1998@yahoo.com>
2020-04-28 20:02:56 -0400

    new exercise in the module folder

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -10,6 +10,7 @@ import javafx.util.Duration;
 import org.context.SessionContext;
 import org.ui.SceneManager;
 
+
 public final class App extends Application {
 
     public static void main(String[] args) {
